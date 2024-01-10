aeca4a0f1a776c62223ebe5c5b9d25355dd5cdee
DioBytes <dyland601@gmail.com>
2024-01-10 13:55:54 -0500

    Two columns made, all pages deleted and transferred to components

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
 // Your JavaScript code
 document.addEventListener("DOMContentLoaded", function () {
-  function loadHTML(url, elementId) {
-    fetch(url)
-        .then((response) => response.text())
-        .then((data) => {
-            document.getElementById(elementId).innerHTML = data;
-        });
-}
-loadHTML('components/layout.html', 'layout-placeholder');
-loadHTML('components/header.html', 'header-placeholder');
-loadHTML('components/footer.html', 'footer-placeholder');
-loadHTML('components/about.html', 'about-placeholder');
-loadHTML('components/socialIcons.html', 'social-icons-placeholder');
+    function loadHTML(url, elementId) {
+        return fetch(url)
+            .then((response) => response.text())
+            .then((data) => {
+                document.getElementById(elementId).innerHTML = data;
+            });
+    }
+
+    async function loadComponents() {
+        try {
+            await loadHTML('components/layout.html', 'layout-placeholder');
+            await loadHTML('components/header.html', 'header-placeholder');
+            await loadHTML('components/identity.html', 'identity-placeholder');
+            await loadHTML('components/nav.html', 'nav-placeholder');
+            await loadHTML('components/socialIcons.html', 'social-icons-placeholder');
+            await loadHTML('components/about.html', 'about-placeholder');
+            await loadHTML('components/achievements.html', 'achievements-placeholder');
+            await loadHTML('components/projects.html', 'projects-placeholder');
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    loadComponents();
 });
