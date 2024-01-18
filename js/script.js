46a24083bd8014cfd472a9ecc50924413a311153
Dylan Dombrowski <dyland601@gmail.com>
2024-01-18 00:14:07 -0500

    started with media queries. left column is almost complete, some minor bugs

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
 // Your JavaScript code
 document.addEventListener("DOMContentLoaded", function () {
-    function loadHTML(url, elementId) {
-        return fetch(url)
-            .then((response) => response.text())
-            .then((data) => {
-                document.getElementById(elementId).innerHTML = data;
-            });
-    }
+  function loadHTML(url, elementId) {
+    return fetch(url)
+      .then((response) => response.text())
+      .then((data) => {
+        document.getElementById(elementId).innerHTML = data;
+      });
+  }
 
-    async function loadComponents() {
-        try {
-            await loadHTML('components/layout.html', 'layout-placeholder');
-            await loadHTML('components/header.html', 'header');
-            await loadHTML('components/identity.html', 'identity-placeholder');
-            await loadHTML('components/nav.html', 'nav-placeholder');
-            await loadHTML('components/socialIcons.html', 'social-icons-placeholder');
-            await loadHTML('components/about.html', 'about-placeholder');
-            await loadHTML('components/achievements.html', 'achievements-placeholder');
-            await loadHTML('components/projects.html', 'projects-placeholder');
-        } catch (error) {
-            console.error('Error:', error);
-        }
+  async function loadComponents() {
+    try {
+      await loadHTML("components/layout.html", "layout-placeholder");
+      await loadHTML("components/header.html", "header");
+      await loadHTML("components/identity.html", "identity-placeholder");
+      await loadHTML("components/nav.html", "nav-placeholder");
+      await loadHTML("components/socialIcons.html", "social-icons-placeholder");
+      await loadHTML("components/about.html", "about-placeholder");
+      await loadHTML(
+        "components/achievements.html",
+        "achievements-placeholder"
+      );
+      await loadHTML("components/projects.html", "projects-placeholder");
+    } catch (error) {
+      console.error("Error:", error);
     }
+  }
 
-    loadComponents();
+  loadComponents();
 });
